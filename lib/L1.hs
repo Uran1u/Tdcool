80ff897426a50a76576ec6691902a920e751c992
Uran1u <Milanjevic1a2b@gmail.com>
2024-12-20 12:22:35 +0000

    sync

--- lib/L1.hs
+++ lib/L1.hs
@@ -3,7 +3,7 @@ Module      : L1
 Description : Verifica colisões no jogo
 Copyright   : [Seu Nome] <seu_email@exemplo.com>
 
-Módulo para a realização da L1 do projeto Tower Defense de LI1 em 2023/24.
+Módulo para a realização da L1 do projeto Tower Defense de LI1 em 2024/25.
 -}
 module L1 where
 
@@ -14,7 +14,7 @@ maptest = Mapa
   { spawnPoint = (0.0, 0.0)
   , endPoint = (5.0, 7.0)
   , grid = [[Ca, Te, Te, Te, Te, Te, Te, Te, Te]
-           ,[Ca, Ca, Ca, Ca, Te, Te, Te, Te, Te]
+           ,[Ca, Ca, Ca, Ca, Te,  Te,Te, Te, Te]
            ,[Te, Te, Te, Ca, Te, Te, Te, Te, Te]
            ,[Te, Te, Te, Ca, Ca, Ca, Ca, Te, Te]
            ,[Te, Te, Te, Te, Te, Te, Ca, Te, Te]
@@ -22,10 +22,14 @@ maptest = Mapa
   }
 -- | Verifica se um projétil colidiu com um inimigo.
 colisaoProjetilInimigo :: Projetil -> Inimigo -> Bool
-colisaoProjetilInimigo Projetil Inimigo = 
-    distancia (posicaoProjetil Projetil) (posicaoInimigo Inimigo) < 0.1
+colisaoProjetilInimigo projetil inimigo =
+    distancia (posicaoProjetil projetil) (posicaoInimigo inimigo) < 0.1
 
 -- | Verifica se um inimigo alcançou a base.
 inimigoNaBase :: Mapa -> Inimigo -> Bool
-inimigoNaBase mapa Inimigo = 
-    distancia (posicaoInimigo Inimigo) (endPoint mapa) < 0.1
+inimigoNaBase mapa inimigo =
+    distancia (posicaoInimigo inimigo) (endPoint mapa) < 0.1
+
+
+
+
